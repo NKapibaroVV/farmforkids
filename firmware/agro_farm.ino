440d896aa8ea4e13c3d1d048b8e551056580ee80
NKapibaroVV <147636976+NKapibaroVV@users.noreply.github.com>
2025-10-30 11:10:21 +0300

    Update agro_farm.ino

--- firmware/agro_farm.ino
+++ firmware/agro_farm.ino
@@ -1,2 +1,29 @@
-// Код Arduino для гидропонной фермы
-// Использование датчиков температуры, влажности и уровня воды
+// Код для Arduino
+// Работа с датчиками температуры (DHT22), уровнем воды (поплавковый датчик)
+// Управление светом (фито-лампа, холодный и тёплый свет), насосом, вентиляторами
+
+#define DHTPIN A1 // Пин для датчика DHT22
+#define DHTTYPE DHT22 // Тип датчика
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(9, OUTPUT); // Пин для холодного света
+  pinMode(10, OUTPUT); // Пин для тёплого света
+  pinMode(8, OUTPUT); // Пин для насоса
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Ошибка чтения с датчика!");
+    return;
+  }
+  Serial.print("Температура: ");
+  Serial.print(temperature);
+  Serial.print(" °C  Влажность: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+}
